06f7b71f9a417f9f9b67d75a5b39ac6a5c28fb07
Jockky <83629006+Jockky-dev@users.noreply.github.com>
2021-10-27 17:23:28 +0700

    Create infinity loop.c

---
+++ infinity loop.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(int argc, char *argv[]) {
+    int i = 1;
+    do{
+        i=i+1;
+        printf("%d",i);
+    }while(i>1);
+}
